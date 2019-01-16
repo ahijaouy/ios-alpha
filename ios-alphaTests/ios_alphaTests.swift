5fcb8bdafb60f16cfab49cfdfc0187567cf15a35
Andre Hijaouy <hijaouya@gmail.com>
2019-01-16 17:23:21 -0500

    Initial Commit

---
+++ ios-alphaTests/ios_alphaTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_alphaTests.swift
+//  ios-alphaTests
+//
+//  Created by Andre Hijaouy on 1/16/19.
+//  Copyright © 2019 Mobile Apps & Services. All rights reserved.
+//
+
+import XCTest
+@testable import ios_alpha
+
+class ios_alphaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
